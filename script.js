4770d1c9b8bea65d7de8e1d5343282252c08ecca
VictorBielski <cph-vb46@cphbusiness.dk>
2018-11-30 09:44:12 +0100

    Layout filer

---
+++ script.js
@@ -0,0 +1,12 @@
+
+/************************************************
+ HAMBURGER MENU ANIMATION 
+************************************************/
+
+$(document).ready(function () {
+    // Hide/show animation hamburger function
+    $('.navbar-toggler').on('click', function () {
+    // Take this line to first hamburger animations
+    $('.animated-icon1').toggleClass('open');
+    });   
+});
