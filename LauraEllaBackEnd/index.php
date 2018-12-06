03db21e74154d31ae203755524ae8429ecccb9e5
Stefan Dimitrijevic <cph-sd77@cphbusiness.dk>
2018-12-06 19:06:59 +0100

    Login index fixed
    Sorry <3

--- LauraEllaBackEnd/index.php
+++ LauraEllaBackEnd/index.php
@@ -1,32 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+	    <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <!-- Material Design Bootstrap -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.15/css/mdb.min.css" rel="stylesheet">
+        <!--Font awesome-->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+        <link rel="stylesheet" href="./css/stylesheet.css"  type="text/css">
+    <title>Lauraella</title>
 </head>
 <body>
-    
-<div class="header">
-	<h1>Laura Ella Bruger Login:</h1>
-</div>
-	<div class="wrapper">	
-	<div class="login">
-		<p>Login:</p>
-	<form action="login.php" method="POST">
-		<div class="form-input">
-		<i class="fas fa-user"></i>
-		<input type="text" name="un" placeholder="Enter Username" required>
-		</div>
-		<div class="form-input">
-		<i class="fas fa-unlock"></i>
-		<input type="password" name="pw" placeholder=" Enter Password" required>
-		</div>
-		<button type="submit">Login</button> 
-	</form>
+	<div class="container-fluid" id="intro">
+  		<div class="row-justify-content center">
+    		<div class="col-12">
+      			<h1 class="intro text-center">Lauraella login</h1>
+    		</div>
+  		</div>
 	</div>
-</div>
 
+	<div class="container-fluid mt-5" id="form">
+  		<div class="row justify-content-center">
+    		<div class="col-lg-4 col-md-8 col-sm-12">
+		
+		<!-- Default form login -->
+      		<form action="login.php" class="text-center border border-light p-5">
+        		<p class="h4 mb-4">Log ind</p>
+        			<input type="email" id="defaultLoginFormEmail" name="un" class="form-control mb-4" placeholder="Brugernavn">
+        			<input type="password" id="defaultLoginFormPassword" name="pw" class="form-control mb-4" placeholder="Kodeord">
+        			<button class="btn btn-info btn-block my-4" type="submit">Sign in</button>
+      		</form>
+    		</div>
+  		</div>
+	</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.15/js/mdb.min.js"></script>
 </body>
 </html>
