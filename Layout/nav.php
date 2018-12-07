1033a208f31e927fcb4211a557b43e59bffd90b4
Victor Bielski <cph-vb46@cphbusiness.dk>
2018-12-07 13:15:04 +0100

    Opdateret layout

--- Layout/nav.php
+++ Layout/nav.php
@@ -30,9 +30,15 @@
     <li class="nav-item active">
       <a class="nav-link" href="#">Forside<span class="sr-only">(current)</span></a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="produkter.php">Produkter</a>
-    </li>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+          aria-expanded="false">Vores varer</a>
+        <div class="dropdown-menu dropdown-primary p-0" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
     <li class="nav-item">
       <a class="nav-link" href="#">Om os</a>
     </li>
