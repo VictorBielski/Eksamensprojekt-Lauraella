907f4db03b4cd46f709eef9351b6d7b479735cc0
Stefan Dimitrijevic <cph-sd77@cphbusiness.dk>
2018-12-08 18:46:17 +0100

    Made the "Om os" page responsible and added a bit of text

--- Layout/omos.php
+++ Layout/omos.php
@@ -11,6 +11,9 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+        <!-- AOS CSS -->
+        <link rel="stylesheet" href="https://unpkg.com/aos@3.0.0-beta.6/dist/aos.css" />
+        <!-- Own stylesheet -->
         <link rel="stylesheet" href="Stylesheets/style.css"  type="text/css">
     <title>Lauraella</title>
   </head>
@@ -34,29 +37,38 @@
 
 
 <section id="omos">
-<div class="container-fluid">
+<div class="container mb-5">
     <div class="row mt-5 justify-content-center">
-        <div class="col-5">
+        <div class="col-sm-12 col-md-12 col-lg-6">
             <img class="img-fluid" id="om_img" src="images/om_img.png">
         </div>
-        <div class="col-5">
+        <div class="col-sm-12 col-md-12 col-lg-6">
             <h1 class="text-center mt-2">Hvad er Lauraella?</h1>
-            <p class="mt-3 text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda corrupti nisi repellat deserunt necessitatibus non totam eaque rem rerum, nihil, velit accusantium delectus hic est voluptas sint in! Necessitatibus, ab. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ipsum totam deserunt nulla nobis quibusdam, ea vero non et, quis optio, recusandae cumque? Doloribus cum, eius accusamus atque temporibus</p>
-            <p class="mt-3 text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda corrupti nisi repellat deserunt necessitatibus non totam eaque rem rerum, nihil, velit accusantium delectus hic est voluptas sint in! Necessitatibus, ab. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ipsum totam deserunt nulla nobis quibusdam, ea vero non et, quis optio, recusandae cumque? Doloribus cum, eius accusamus atque temporibus</p>
+            <p class="mt-3 text-center">Laura Ella er en spændende brugskunstbutik, som har belligenhed i Dragør. Butikken rummer en masse delikatesser og vine, og du får en oplevelse ud over det sædvanlige. Vi laver flotte værtindegaver, firmagaver, venindegaver, kærlighedsgaver samt forkælelsesgaver - alt efter netop dine behov.</p>
+            <p class="mt-3 text-center">Laura Ella byder også på mange lækre ting til hjemmet. Puder i de skønneste farver, det populøre porcelæn fra "Pip" og meget mere. Til forskønnelse af krop og sjæl forhander vi det evigt populøre "Durance" fra Frankrig, som laver velværeprodukter, lavet på den rene natur.</p>
+        </div>
+
+<!-- Row end -->
     </div>
-</div>
 
     <div class="row mt-5 justify-content-center">
-        <div class="col-5">
+
+        <div class="col-sm-12 col-md-12 col-lg-6 order-lg-1">
+            <img class="img-fluid" id="om_img" src="images/om_img.png">
+        </div>
+
+        <div class="col-sm-12 col-md-12 col-lg-6">
             <h1 class="text-center mt-2">Brugskunst siden "2010"</h1>
             <p class="mt-3 text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda corrupti nisi repellat deserunt necessitatibus non totam eaque rem rerum, nihil, velit accusantium delectus hic est voluptas sint in! Necessitatibus, ab. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ipsum totam deserunt nulla nobis quibusdam, ea vero non et, quis optio, recusandae cumque? Doloribus cum, eius accusamus atque temporibus</p>
             <p class="mt-3 text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda corrupti nisi repellat deserunt necessitatibus non totam eaque rem rerum, nihil, velit accusantium delectus hic est voluptas sint in! Necessitatibus, ab. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ipsum totam deserunt nulla nobis quibusdam, ea vero non et, quis optio, recusandae cumque? Doloribus cum, eius accusamus atque temporibus</p>
         </div>
-        <div class="col-5">
-            <img class="img-fluid" id="om_img" src="images/om_img.png">
-        </div>
+
+        <!-- Row end -->
     </div>
+
+<!-- Container end -->
 </div>
+
 </section>
 <?php 
     include 'footer.php';
@@ -71,5 +83,13 @@
     <!-- MDB core JavaScript -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.15/js/mdb.min.js"></script>
     <script src="scripts/script.js" type="text/javascript"></script>
+    <!-- AOS animation JS -->
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+	
+    <script>	
+        // Script for the fade-up animation
+        AOS.init();
+    </script>
+
   </body>
 </html>
