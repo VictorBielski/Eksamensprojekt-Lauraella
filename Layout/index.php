ca9d08e7c1a771839931f8446a5f90b2d896d253
Victor Bielski <cph-vb46@cphbusiness.dk>
2018-12-11 14:17:59 +0100

    Slider og parrallax fjernet - 3 rows med 3 billeder i hver tilføjet til galleri

--- Layout/index.php
+++ Layout/index.php
@@ -32,52 +32,6 @@
     include 'nav.php';
 ?>
 
-<div class="container-fluid p-0" id="baggrund">
-
-<section id="landing">
-  <div class="container-fluid mt-2">
-    <div class="row">
-      <div class="col-12 p-0">
-        <!-- IMAGE SLIDER START -->
-        <div id="img_carousel" class="carousel slide carousel-fade" data-ride="carousel">
-          <!--INDICATORS START-->
-          <ol class="carousel-indicators">
-            <li data-target="#img_carousel" data-slide-to="0" class="active"></li>
-            <li data-target="#img_carousel" data-slide-to="1"></li>
-            <li data-target="#img_carousel" data-slide-to="2"></li>
-          </ol>
-          <!--INDICATORS SLUT-->
-
-          <!--Slides-->
-          <div class="carousel-inner" role="listbox">
-            <!--First slide-->
-            <div class="carousel-item active">
-              <img class="d-block w-100" src="images/slider_1.jpg" alt="First slide">
-            </div>
-            <!--/First slide-->
-            <!--Second slide-->
-            <div class="carousel-item">
-              <img class="d-block w-100" src="images/slider_1.jpg" alt="Second slide">
-            </div>
-            <!--/Second slide-->
-            <!--Third slide-->
-            <div class="carousel-item">
-              <img class="d-block w-100" src="images/slider_1.jpg" alt="Third slide">
-            </div>
-            <!--/Third slide-->
-          <a class="carousel-control-prev" href="#img_carousel" role="button" data-slide="prev">
-          <i class="fas fa-angle-left fa-3x"></i>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#img_carousel" role="button" data-slide="next">
-          <i class="fas fa-angle-right fa-3x"></i>
-            <span class="sr-only">Next</span>
-          </a>
-          </div>
-        </div>
-      </div>
-    </div>
-</section>
 
 <section id="mini_gallery">
   <div class="container">
@@ -91,25 +45,34 @@
       <div class="col-lg-3 col-md-3 col-sm-6 mini_gallery">
           <img class="img-fluid" src="images/galleri_1.jpg">
       </div>
-      <div class="col-lg-3 col-md-3 col-sm-6 mini_gallery"> 
+    </div>
+
+        <div class="row mt-5 justify-content-center">
+      <div class="col-lg-3 col-md-3 col-sm-6 mini_gallery">
           <img class="img-fluid" src="images/galleri_1.jpg">
       </div>
-
-    </div>
-</div>
-</section>
-</div>
-
-<section id="about">
-  <div class="container-fluid mt-5 m-0 rgba-cyan-slight" id="overlay">
-    <div class="row">
-      <div class="col-10">
-          <h1>Lauraella</h1>
-          <h3>Vi skræddersyer produkter efter dine behov</h3>
+      <div class="col-lg-3 col-md-3 col-sm-6 mini_gallery">
+          <img class="img-fluid" src="images/galleri_1.jpg">
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-6 mini_gallery">
+          <img class="img-fluid" src="images/galleri_1.jpg">
       </div>
     </div>
- </div>
+    <div class="row mt-5 justify-content-center mb-5">
+      <div class="col-lg-3 col-md-3 col-sm-6 mini_gallery">
+          <img class="img-fluid" src="images/galleri_1.jpg">
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-6 mini_gallery">
+          <img class="img-fluid" src="images/galleri_1.jpg">
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-6 mini_gallery">
+          <img class="img-fluid" src="images/galleri_1.jpg">
+      </div>
+    </div>
+</div>
 </section>
+
+
 
 
 <?php 
