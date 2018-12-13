46ed1c25dcca1ae3612c069c4162e451edc98e7f
Victor Bielski <cph-vb46@cphbusiness.dk>
2018-12-13 11:11:59 +0100

    Opdateret

--- Layout/index.php
+++ Layout/index.php
@@ -23,6 +23,51 @@
     include 'nav.php';
 ?>
 
+<section id="landing">
+  <div class="container-fluid mt-2">
+    <div class="row">
+      <div class="col-12 p-0">
+        <!-- IMAGE SLIDER START -->
+        <div id="img_carousel" class="carousel slide carousel-fade" data-ride="carousel">
+          <!--INDICATORS START-->
+          <ol class="carousel-indicators">
+            <li data-target="#img_carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#img_carousel" data-slide-to="1"></li>
+            <li data-target="#img_carousel" data-slide-to="2"></li>
+          </ol>
+          <!--INDICATORS SLUT-->
+
+          <!--Slides-->
+          <div class="carousel-inner" role="listbox">
+            <!--First slide-->
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="images/slider_1.jpg" alt="First slide">
+            </div>
+            <!--/First slide-->
+            <!--Second slide-->
+            <div class="carousel-item">
+              <img class="d-block w-100" src="images/slider_1.jpg" alt="Second slide">
+            </div>
+            <!--/Second slide-->
+            <!--Third slide-->
+            <div class="carousel-item">
+              <img class="d-block w-100" src="images/slider_1.jpg" alt="Third slide">
+            </div>
+            <!--/Third slide-->
+          <a class="carousel-control-prev" href="#img_carousel" role="button" data-slide="prev">
+          <i class="fas fa-angle-left fa-3x"></i>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#img_carousel" role="button" data-slide="next">
+          <i class="fas fa-angle-right fa-3x"></i>
+            <span class="sr-only">Next</span>
+          </a>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+
 
 <section id="mini_gallery">
   <div class="container">
@@ -131,11 +176,6 @@
     </div>
   </div>
 </div>
-
-
-
-
-
 </div>
 </section>
 
--- Layout/scripts/script.js
+++ Layout/scripts/script.js
@@ -11,20 +11,27 @@ $(document).ready(function () {
     });   
 });
 
-$(window).scroll(function() {
-    if ($(this).scrollTop() < 200) {
-        $("footer").slideUp();
-    }
-    else {
-        $("footer").slideDown();
-    }
-});
-
-var rellax = new Rellax('.rellax', {
-    speed: -0.5,
-    center: false,
-    wrapper: null,
-    round: true,
-    vertical: true,
-    horizontal: false
-  });
+// This code dosen't works on Firefox and IE and works on other browesers.
+$(document).ready(function () {
+    $('.animated-icon1,.animated-icon3,.animated-icon4').click(function () {
+    $(this).toggleClass('open');
+    });
+    });
+    
+    // Works everywhere
+    $(document).ready(function () {
+    
+    // Hide/show animation hamburger function
+    $('.navbar-toggler').on('click', function () {
+    
+    // Take this line to first hamburger animations
+    $('.animated-icon1').toggleClass('open');
+    
+    // Take this line to second hamburger animation
+    $('.animated-icon3').toggleClass('open');
+    
+    // Take this line to third hamburger animation
+    $('.animated-icon4').toggleClass('open');
+    });
+    
+    });
