4dc126434733fe38f1143b446f9dee15ba4e685f
Stefan Dimitrijevic <cph-sd77@cphbusiness.dk>
2018-12-14 19:48:09 +0100

    Update footer.php

--- Layout/footer.php
+++ Layout/footer.php
@@ -11,30 +11,30 @@
   <body>
 <footer class="card-footer" id="footer">
 <div class="container-fluid">
-  <div class="row justify-content-center">
-    <div class="col-lg-4 col-md-4 col-sm-12 mt-4 text-center">
-        <ul class="text-center">
+  <div class="row justify-content-center text-center">
+    <div class="col-sm-12 col-md-4 col-lg-4 mt-4 ">
+        <ul class="">
         <img class="åbningstider_ikon" src="/Eksamensprojekt-Lauraella/Layout/images/ikoner/opening-hours.png">
         <p class="mt-3">Mandag - Fredag..... 10.00-18.00</p>
         <p class="mt-1">Lørdag....................... 10.00-14.00</p>
         <p class="mt-1">Søndag.......................10.00-16.00</p>
         </ul>
     </div>
-  <div class="col-lg-4 col-md-4 col-sm-12 mt-4 text-center">
+  <div class="col-sm-12 col-md-4 col-lg-4 mt-4 ">
     <ul>
-    <i class="fas fa-map-marker-alt text-center fa-3x"></i>
+    <i class="fas fa-map-marker-alt  fa-3x"></i>
       <li class="mt-3"><a href="#">Kongevejen 13</a></li>
       <li class="mt-1"><a href="#">2791 Dragør</a></li>
       <li class="mt-1"><a href="#">Danmark</a></li>
     </ul>
   </div>
-  <div class="col-lg-4 col-md-4 col-sm-12 text-center mt-4">
+  <div class="col-sm-12 col-md-4 col-lg-4 mt-4">
     <h4 class="ml-5 mt-2">Kontakt</h4>
-    <p class="phone mt-4 text-center ml-4">
+    <p class="phone mt-4 ml-4">
     <i class="fas fa-phone-square fa-2x mr-4"></i>+45 32 53 00 18</p>
     </p>
     <p class="phone mt-3 text-cente ml-4">
-    <i class="fas fa-envelope-square fa-2x mr-2"></i>helle@gmail.com</p>
+    <i class="fas fa-envelope-square fa-2x mr-1"></i>helle@gmail.com</p>
     </p>
   </div>
   </div>
