285ccd571e17a0415c7d6c42b30afe6225c8fa92
Victor Bielski <cph-vb46@cphbusiness.dk>
2018-12-15 22:25:16 +0100

    Styleguide Idd fil

--- Layout/nav.php
+++ Layout/nav.php
@@ -30,8 +30,8 @@
         <h3><a href="index.php" class="logo">LAURA ELLA</a></h3>
         <p>~ Alt hvad vi har i Laura Ella er unødvendigt, det gør bare livet lidt hyggeligere. ~</p>
     </div>
-</div>
   </div>
+</div>
 
 <div class="container-fluid p-0"> 
 <nav class="navbar navbar-expand-md mt-3">
